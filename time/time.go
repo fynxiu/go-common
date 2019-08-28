1d91efb79358bd8e3bb3d302e3afb8cbc1ae2ce5
fynxiu <fyn.xiu@gmail.com>
2019-08-28 21:07:00 +0800

    feat: time add format layout

--- time/time.go
+++ time/time.go
@@ -7,6 +7,15 @@ import (
 	xtime "time"
 )
 
+const (
+	LayoutStandard       = "2006-01-02 15:04:05"
+	LayoutStandardMinute = "2006-01-02 15:04"
+	LayoutStandardMil    = "2006-01-02 15:04:05.999999"
+	LayoutStandardShort  = "2006-01-02"
+	LayoutSlash          = "2006/01/02 15:04:05"
+	LayoutSlashShort     = "2006/01/02"
+)
+
 // Time be used to MySql timestamp converting.
 type Time int64
 
