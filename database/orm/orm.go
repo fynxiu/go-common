51667d3c3083a9decca191389e145ded87ac4139
fynxiu <fyn.xiu@gmail.com>
2019-08-28 16:26:46 +0800

    refactor: remove log package dependency

--- database/orm/orm.go
+++ database/orm/orm.go
@@ -1,11 +1,10 @@
 package orm
 
 import (
+	xtime "github.com/fynxiu/go-common/time"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-	"github.com/fynxiu/go-common/log"
-	xtime "github.com/fynxiu/go-common/time"
-	"strings"
+	"log"
 	"time"
 )
 
@@ -17,13 +16,6 @@ type Config struct {
 	IdleTimeout xtime.Duration // idle connection max life time
 }
 
-type ormLog struct{}
-
-// Print ormLog print
-func (r ormLog) Print(v ...interface{}) {
-	log.Info(strings.Repeat("%v ", len(v)), v...)
-}
-
 func init() {
 	// TODO: custom error message
 	// gorm.ErrRecordNotFound = ecode.NothingFound
@@ -33,12 +25,10 @@ func init() {
 func NewMySQL(c *Config) (db *gorm.DB) {
 	db, err := gorm.Open("mysql", c.DSN)
 	if err != nil {
-		_ = log.Error("db dsn(%s) error: %v", c.DSN, err)
-		panic(err)
+		log.Fatalf("db dsn(%s) error: %v", c.DSN, err)
 	}
 	db.DB().SetMaxIdleConns(c.Idle)
 	db.DB().SetMaxOpenConns(c.Active)
 	db.DB().SetConnMaxLifetime(time.Duration(c.IdleTimeout) / time.Second)
-	db.SetLogger(ormLog{})
 	return
 }
