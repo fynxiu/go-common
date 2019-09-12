71b6060e499c02f652d241b3a9783a563ea0b0ed
fynxiu <fyn.xiu@gmail.com>
2019-09-12 21:34:29 +0800

    feat: iter.N

---
+++ sugar/iter/iter_test.go
@@ -0,0 +1,33 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package iter_test
+
+import (
+	"fmt"
+	"github.com/fynxiu/go-common/sugar/iter"
+	"testing"
+)
+
+func ExampleN() {
+	for i := range iter.N(4) {
+		fmt.Println(i)
+	}
+	// Output:
+	// 0
+	// 1
+	// 2
+	// 3
+}
+
+func TestAllocs(t *testing.T) {
+	var sink []struct{}
+	allocs := testing.AllocsPerRun(1000, func() {
+		sink = iter.N(1e9)
+	})
+	_ = sink
+	if allocs > 0.1 {
+		t.Errorf("allocs = %v", allocs)
+	}
+}
